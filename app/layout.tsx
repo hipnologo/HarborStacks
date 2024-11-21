b8ea206fd9b00d6ab0c14c497407447299d472a5
Fabio Carvalho <Fabio_Carvalho@comcast.com>
2024-11-20 19:54:23 -0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@
 // }
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '@/styles/globals.css';
+import './globals.css';
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
